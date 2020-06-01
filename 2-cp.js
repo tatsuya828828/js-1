c2e54f1832f20fe64f6b74a3012d4944a5a32da3
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-06-01 20:17:53 +0900

    [finish]js-2

---
+++ 2-cp.js
@@ -0,0 +1,7 @@
+var int1 = 5
+var int2 = 8
+
+alert(int1+'+'+int2+'は'+(int1+int2)+'です');
+alert(int1+'-'+int2+'は'+(int1-int2)+'です');
+alert(int1+'×'+int2+'は'+(int1*int2)+'です');
+alert(int1+'÷'+int2+'は'+(int1/int2)+'です')
