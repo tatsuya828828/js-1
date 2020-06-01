debc197fed47dcdcf362d39ff3883a78cb37f41a
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-06-01 19:53:53 +0900

    [add] js-2-2

---
+++ js-2-2.js
@@ -0,0 +1,12 @@
+var orange = 150;
+var apple = 120;
+
+if(orange < apple){
+    alert('みかんの値段がリンゴより安い');
+}
+else if(orange == apple){
+    alert('みかんとリンゴが同じ値段');
+}
+else{
+    alert('みかんの値段がリンゴより高い');
+}
